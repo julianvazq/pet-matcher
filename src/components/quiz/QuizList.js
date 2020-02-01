d759c118e7adae750d7f4fdb3ed6f31b23cddd44
Julian Vazquez <julianvazquez101@gmail.com>
2020-02-01 14:06:19 -0500

    Implement inset shadows / add background

--- src/components/quiz/QuizList.js
+++ src/components/quiz/QuizList.js
@@ -146,6 +146,13 @@ const QuizList = props => {
   const StyledQuizList = styled.div`
     max-width: 600px;
     margin: 2rem 1rem;
+    padding: 2rem;
+    border-radius: 0.5rem;
+    background: hsl(220, 40%, 90%);
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.07),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.05), 0 12.5px 10px rgba(0, 0, 0, 0.042),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.035), 0 41.8px 33.4px rgba(0, 0, 0, 0.028),
+      0 100px 80px rgba(0, 0, 0, 0.02);
 
     @media (min-width: 500px) {
       margin: 2rem auto;
@@ -161,6 +168,8 @@ const QuizList = props => {
     color: hsl(255, 32%, 35%);
     background: hsl(255, 32%, 75%);
   `;
+
+  // <a href="https://www.vecteezy.com/free-vector/id-card-background">Id Card Background Vectors by Vecteezy</a>
 
   return (
     <StyledQuizList>
--- src/components/quiz/form-styles.js
+++ src/components/quiz/form-styles.js
@@ -5,6 +5,32 @@ export const QuestionContainer = styled.div`
   padding: 1.5rem;
   background: hsl(220, 34%, 75%);
   border-radius: 0.5rem;
+  position: relative;
+  z-index: 0;
+
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 0.5rem;
+    box-shadow: inset 0px 1px 3px 0px hsla(220, 34%, 25%, 0.2);
+    z-index: -1;
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 0.5rem;
+    box-shadow: inset 0 -2px 0 hsla(220, 50%, 90%, 0.7);
+    z-index: -1;
+  }
 `;
 
 export const Label = styled.label`
