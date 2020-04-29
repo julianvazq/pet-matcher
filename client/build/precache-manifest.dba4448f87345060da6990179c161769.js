8cdfd27314f13dffe1cb321a96a847e6fb892359
Julian Vazquez <julianvazquez101@gmail.com>
2020-04-28 22:40:12 -0400

    Build

---
+++ client/build/precache-manifest.dba4448f87345060da6990179c161769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f61feb4e12a71c3fba5359d9d493da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d56aa2e62b3fd44cdbd1",
+    "url": "/static/css/main.4013889b.chunk.css"
+  },
+  {
+    "revision": "04b9ce0364f217f5e4b3",
+    "url": "/static/js/2.8ca15ea2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8ca15ea2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d56aa2e62b3fd44cdbd1",
+    "url": "/static/js/main.c3016425.chunk.js"
+  },
+  {
+    "revision": "5aacbaacdfb1cd567062",
+    "url": "/static/js/runtime-main.8e720e6c.js"
+  },
+  {
+    "revision": "e8088bc4ca893bd83d423d26a02dfc7a",
+    "url": "/static/media/dog-pattern-1000.e8088bc4.jpg"
+  }
+]);
